cb2ff70c3c960390c64da395ffef7800eaa2e852
planttheidea <tony.quetano@planttheidea.com>
2020-02-15 15:04:18 -0800

    remove deprecated use of resolve rollup plugin

--- rollup.config.js
+++ rollup.config.js
@@ -1,44 +1,42 @@
 import babel from 'rollup-plugin-babel';
 import resolve from 'rollup-plugin-node-resolve';
-import {uglify} from 'rollup-plugin-uglify';
+import { uglify } from 'rollup-plugin-uglify';
 
 export default [
   {
     input: 'src/index.js',
     output: {
       exports: 'named',
-      name: 'hashIt',
       file: 'dist/hash-it.js',
       format: 'umd',
-      sourcemap: true
+      name: 'hashIt',
+      sourcemap: true,
     },
     plugins: [
       resolve({
-        module: true,
-        main: true
+        mainFields: ['module', 'main'],
       }),
       babel({
-        exclude: 'node_modules/**'
-      })
-    ]
+        exclude: 'node_modules/**',
+      }),
+    ],
   },
   {
     input: 'src/index.js',
     output: {
       exports: 'named',
-      name: 'hashIt',
       file: 'dist/hash-it.min.js',
-      format: 'umd'
+      format: 'umd',
+      name: 'hashIt',
     },
     plugins: [
       resolve({
-        module: true,
-        main: true
+        mainFields: ['module', 'main'],
       }),
       babel({
-        exclude: 'node_modules/**'
+        exclude: 'node_modules/**',
       }),
-      uglify()
-    ]
-  }
+      uglify(),
+    ],
+  },
 ];
