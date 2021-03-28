a34af6e0dfd7f36466600bf7d6d858cb0319c010
Tony Quetano <tony.quetano@planttheidea.com>
2021-03-28 15:26:59 -0700

    fix grammar in test name

--- __tests__/hash.ts
+++ __tests__/hash.ts
@@ -1,7 +1,7 @@
 import hash from '../src/hash';
 
 describe('hash', () => {
-  it('should have return the correct value', () => {
+  it('should return the correct value', () => {
     const string = 'foo';
 
     expect(hash(string)).toBe(794144147649);
